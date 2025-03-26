73bbf5d5ed6fdc6b1abfee39d2b8045814569694
oussamaamou <amoussdark@gmail.com>
2025-03-26 01:21:37 +0000

    Configuration des nouveaux packages installes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function(AuthenticationException $e, Request $request){
