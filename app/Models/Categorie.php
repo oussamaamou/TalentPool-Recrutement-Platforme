52017f5de843eea2a3c892140b507f9fd383c04f
oussamaamou <amoussdark@gmail.com>
2025-03-26 00:07:23 +0000

    Creation du model de categorie

---
+++ app/Models/Categorie.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Categorie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function annonces(): HasMany
+    {
+        return $this->hasMany(Annonce::class);
+    }
+}
