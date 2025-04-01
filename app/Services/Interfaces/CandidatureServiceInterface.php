e555d5b1a97888cf58f15e5a01cec489e2480ad6
oussamaamou <amoussdark@gmail.com>
2025-04-01 02:12:19 +0000

    Ajout du Service Interface

--- app/Services/Interfaces/CandidatureServiceInterface.php
+++ app/Services/Interfaces/CandidatureServiceInterface.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 
 namespace App\Services\Interfaces;
 
 use App\Models\Candidature;
 use Illuminate\Database\Eloquent\Collection;
 
-interface CandidatureRepositoryInterface
+interface CandidatureServiceInterface
 {
     public function getAllCandidatures(): Collection;
     public function getCandidatureById(int $id): ?Candidature;
     public function createCandidature(array $data): Candidature;
     public function updateCandidature(int $id, array $data): ?Candidature;
     public function deleteCandidature(int $id): bool;
-    public function getCandidaturesByCandidat(int $candidatId): Collection;
     public function getCandidaturesByAnnonce(int $annonceId): Collection;
-    public function updateCandidatureStatut(int $id, string $statut): ?Candidature;
-    public function getCandidaturesByRecruteurAnnonces(int $recruteurId): Collection;
+    public function getCandidaturesByCandidat(int $candidatId): Collection;
+    public function updateCandidatureStatus(int $id, string $statut): ?Candidature;
 }
