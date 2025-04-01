3f5dea17b766b2b29c5acf380dba23a29b8a0d7e
oussamaamou <amoussdark@gmail.com>
2025-04-01 02:14:01 +0000

    Ajout du Repository Service Provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -6,6 +6,17 @@ use App\Repositories\AnnonceRepository;
 use App\Repositories\Interfaces\AnnonceRepositoryInterface;
 use App\Services\AnnonceService;
 use App\Services\Interfaces\AnnonceServiceInterface;
+
+use App\Repositories\Interfaces\CandidatureRepositoryInterface;
+use App\Repositories\CandidatureRepository;
+use App\Services\Interfaces\CandidatureServiceInterface;
+use App\Services\CandidatureService;
+
+use App\Repositories\Interfaces\CategorieRepositoryInterface;
+use App\Repositories\CategorieRepository;
+use App\Services\Interfaces\CategorieServiceInterface;
+use App\Services\CategorieService;
+
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -15,11 +26,17 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Binding des repositories
+        // Annonces
         $this->app->bind(AnnonceRepositoryInterface::class, AnnonceRepository::class);
-        
-        // Binding des services
         $this->app->bind(AnnonceServiceInterface::class, AnnonceService::class);
+
+        // Candidatures
+        $this->app->bind(CandidatureRepositoryInterface::class, CandidatureRepository::class);
+        $this->app->bind(CandidatureServiceInterface::class, CandidatureService::class);
+
+        // Categorie
+        $this->app->bind(CategorieRepositoryInterface::class, CategorieRepository::class);
+        $this->app->bind(CategorieServiceInterface::class, CategorieService::class);
     }
 
     /**
