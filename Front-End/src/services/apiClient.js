e02ec533c20b578d8ecf357ed219b32e23de25e6
oussamaamou <amoussdark@gmail.com>
2025-04-14 10:23:52 +0100

    Integration des services des APIs

---
+++ Front-End/src/services/apiClient.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
